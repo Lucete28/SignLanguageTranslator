046af79d2e2554d52d6e1767312d8a1864bfaa7f
Ho Yeon Jeong <2580jhy@naver.com>
2024-03-22 18:56:01 +0900

    인터페이스 구축

--- interface/App.py
+++ interface/App.py
@@ -1,15 +1,20 @@
+#TODO
+# 시작값 해결
+#(선택) 단어 판별 로직 추가
 import streamlit as st
 import cv2
 import mediapipe as mp
 import numpy as np
 from tensorflow.keras.models import load_model
-
+import pickle
+import json
 
 seq_length = 30
-pred_idx = 0
+pred_idx = -1
 CANT_FIND_HAND_COUNT = 0
-# set model
-model = load_model(f"C:/PlayData/lstm_test43_1439act_e20_C8_B512.h5")
+
+
+
 # set sessions
 if 'camera' not in st.session_state:
     st.session_state.camera = False
@@ -18,7 +23,29 @@ if 'frame_count' not in st.session_state:
 if 'data_poket' not in st.session_state:
     st.session_state.data_poket = np.zeros((1, 156))
 if 'sentence' not in st.session_state:
-    st.session_state.sentence = [0]
+    st.session_state.sentence = [None]
+if 'actions' not in st.session_state:
+    with open(r'G:\내 드라이브\LAB\SignLanguageTranslator\logs\act_pkl\V2_A300.pkl', 'rb') as file:
+        st.session_state.actions = pickle.load(file)
+        print(len(st.session_state.actions),'개의 액션이 저장되어있습니다.')
+        st.session_state.actions.append(None)
+if 'j_data' not in st.session_state:
+    with open(r'G:\내 드라이브\LAB\SignLanguageTranslator\logs\api_log.json', 'r',encoding='utf-8') as j_file:
+        st.session_state.j_data = json.load(j_file)
+        print('원본 로드 성공')
+if 'model' not in st.session_state:
+    st.session_state.model = load_model(f"C:/PlayData/lstm_test_V2_A300_e50_C0_B0.h5") 
+if 'sentence_record' not in st.session_state:
+    st.session_state.sentence_record = []
+    
+##### define function
+def translate_e_to_k(word):
+    for page in st.session_state.j_data['Daily']:
+        if word in st.session_state.j_data['Daily'][page]:
+            return st.session_state.j_data['Daily'][page][word][0]
+        
+def re_run():
+    st.experimental_rerun()
 # MediaPipe 손 인식 초기화
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
@@ -32,13 +59,23 @@ st.title("수어 인식 및 번역")
 c1, c2 = st.columns(2)
 if c1.button('Start'):
     st.session_state.camera = True
+    re_run()
 
 frame_placeholder = st.empty()
 word_placeholder = st.empty()
-state_placeholder = st.empty()
+c11,c12 = st.columns(2)
+
+if st.session_state.camera and c12.button('Turn to Sentence'):
+    c12.write([i for i in st.session_state.sentence if i is not None])
+    st.session_state.sentence_record.append([i for i in st.session_state.sentence if i is not None])
+    st.session_state.sentence= [None]
+sentence_placeholder = c11.empty()
 if c2.button('End'):
     st.session_state.camera = False
-    # st.session_state.frame_count = 0  # 카메라가 꺼질 때 FRAME_COUNT 초기화
+    if st.session_state.sentence:
+        st.session_state.sentence_record.append([i for i in st.session_state.sentence if i is not None])
+        st.session_state.sentence= [None]
+    re_run()
 
 if st.session_state.camera:
     cap = cv2.VideoCapture(0)
@@ -88,22 +125,28 @@ if st.session_state.camera:
                     st.session_state.data_poket = np.vstack([st.session_state.data_poket,np.concatenate(da)])
                     if st.session_state.data_poket.shape[0] >= 30:
                         input_data = np.expand_dims(np.array(st.session_state.data_poket[-seq_length:], dtype=np.float16), axis=0)
-                        pred = model.predict(input_data, verbose=0).squeeze()
+                        pred = st.session_state.model.predict(input_data, verbose=0).squeeze()
                         pred_idx= int(np.argmax(pred))
+                        if st.session_state.data_poket.shape[0] >= 100:
+                            st.session_state.data_poket = st.session_state.data_poket[:30]
+                        
                 else:
                     st.session_state.data_poket = da
         else: #손이 보이지 않을때
             CANT_FIND_HAND_COUNT +=1
-            if CANT_FIND_HAND_COUNT >= 30 and st.session_state.sentence[-1]!=pred_idx:
-                st.session_state.sentence.append(pred_idx)
-
-
+            if CANT_FIND_HAND_COUNT >= 10 and st.session_state.sentence[-1]!=translate_e_to_k(st.session_state.actions[pred_idx]):
+                st.session_state.sentence.append(translate_e_to_k(st.session_state.actions[pred_idx]))
+                pred_idx =-1
 
         frame_placeholder.image(frame, channels="BGR", use_column_width=True)
-        word_placeholder.write(f'WORD: {pred_idx}')
-        state_placeholder.write(st.session_state.sentence)
-    cap.release()
-    hands.close()
+        word_placeholder.write(f'WORD: {translate_e_to_k(st.session_state.actions[pred_idx])}, {st.session_state.actions[pred_idx]}')
+        sentence_placeholder.write([i for i in st.session_state.sentence if i is not None])
+if not st.session_state.camera:
+    try:
+        cap.release()
+        hands.close()
+    except: 
+        pass
 
 # 카메라 작동 여부와 관계없이 FRAME_COUNT 표시
-st.write(f'FRAME_COUNT: {st.session_state.frame_count}')
+st.write(f'record: {st.session_state.sentence_record}') 
