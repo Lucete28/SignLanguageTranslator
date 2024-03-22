9fc04fd68210eac5fcbfe3c80006c1cfc389d811
dla1234wns <dla1234wns@naver.com>
2024-03-22 12:24:53 +0900

    start+

---
+++ code/lstm/FAST_API.py
@@ -0,0 +1,125 @@
+# cd C:\Users\oem\Desktop\jhy\signlanguage\Sign_Language_Remaster\code\lstm; uvicorn FAST_API:app --reload --host 0.0.0.0
+# .\apienv\Scripts\activate
+# http:203.250.133.192:8000/
+
+# 사전준비
+import sys
+print("Python version")
+print(sys.version)
+try:
+    import tensorflow as tf
+    print("TensorFlow is installed")
+    print(tf.__version__)
+except ImportError:
+    print("TensorFlow is not installed")
+import csv
+with open("keys.csv", "r") as file:
+    csv_reader = csv.reader(file)
+    
+    # 각 행의 첫 번째 열만 추출하여 리스트로 변환
+    key_list = [row[0] for row in csv_reader]
+
+from tensorflow.keras.models import load_model
+from fastapi import FastAPI, Request,HTTPException
+from pydantic import BaseModel
+import numpy as np
+from collections import Counter
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
+GROUP_SIZE = 25
+MODELS = []
+for i in range(GROUP_SIZE):
+    print(f'{i+1}/{GROUP_SIZE}')
+    model = load_model(f'C:/Users/oem/Desktop/jhy/signlanguage/Sign_Language_Remaster/model/2024-02-25_23-26-15/lstm_test103_G{i}_1645act_e20_C2_B0.h5')
+    MODELS.append(model)
+print('All models ready')
+app = FastAPI()
+
+
+@app.get("/")
+def test():
+    return {"message": "Hello World"}
+
+class Item(BaseModel):
+    array: list  # 넘파이 배열을 리스트로 받음
+
+
+
+# async def predict_model(model, array):
+#     start_time = time.time()
+#     pred = model.predict(array, verbose=0).squeeze()
+#     duration = time.time() - start_time
+#     return np.argmax(pred), duration
+
+re_li =[ [] for _ in range(GROUP_SIZE) ]
+
+# @app.post("/receive")
+# async def receive_array(request: Request):
+#     # 데이터 받아서 변환
+#     data = await request.json()
+#     array_list = data['array']
+#     array = np.array(array_list, dtype=np.float16)
+#     for i, model in enumerate(MODELS):
+#         pred = model.predict(array, verbose=0).squeeze()        
+#         re_li[i].append(int(np.argmax(pred)))
+#         #TODO conf 확인해서 처리 하도록(0.9이상?)
+#     return {"status": "array received", "shape": array.shape, "CODE" : True}
+
+#############################################################
+def model_predict(model, array):
+    pred = model.predict(array, verbose=0).squeeze()
+    return int(np.argmax(pred))
+
+@app.post("/receive")
+async def receive_array(request: Request):
+    data = await request.json()
+    array_list = data['array']
+    array = np.array(array_list, dtype=np.float16)
+
+    with ThreadPoolExecutor(max_workers=25) as executor:
+        future_to_model = {executor.submit(model_predict, model, array): i for i, model in enumerate(MODELS)}
+        for future in as_completed(future_to_model):
+            model_index = future_to_model[future]
+            try:
+                result = future.result()
+            except Exception as exc:
+                return {"CODE": False, "status": f'Model {model_index} generated an exception: {exc}'}
+            else:
+                re_li[model_index].append(result)
+    print(len(re_li[0]),re_li[0])
+    return {"status": "array received", "shape": array.shape, "CODE": True, "tmp" : re_li[0]}
+###############################################################
+
+
+
+
+@app.get("/confirm")
+def confirm():
+    organize_li = []
+    result =[]
+    act_len = 0
+    for re in re_li:
+        if re:
+            most_common_num, most_common_count = Counter(re).most_common(1)[0]
+            organize_li.append(most_common_num)
+            re.clear()
+            result.append([re,most_common_num,most_common_count])
+            act_len = len(re)
+    if organize_li:
+        final_confrim_li = Counter(organize_li).most_common()
+
+        # for li in re_li:
+        #     li.clear()
+        return {"status": "Hello World","CODE":True, "pred_count" : final_confrim_li, "most_common_pred" : final_confrim_li[0][0], "most_common_count": final_confrim_li[0][1],"is_array_here":False,"most_common_by_model":result,"action_len":act_len}
+    else:
+        return {"status" : "NO DATA", "CODE":False}
+    
+
+
+@app.post("/certification")
+def receive_array(key: str):
+    if key not in key_list:
+        raise HTTPException(status_code=401, detail="Invalid API Key")
+    return {"CODE":True,"message": "Successfully authenticated"}
---
+++ code/lstm/run_generator.py
@@ -0,0 +1,119 @@
+import requests
+import xmltodict
+import json
+from data_generator import trans_to_english
+from datetime import datetime
+from data_generator import *
+# import datetime
+#####################################################################################################
+
+def page_todo(file_path, key):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as json_file:
+            data = json.load(json_file)
+    except FileNotFoundError:
+
+        data = {}
+
+    if key not in data: # key 없을때 
+
+        data[key] = dict()
+
+
+        with open(file_path, 'w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, ensure_ascii=False, indent=4)
+            print(f"{file_path}에 키 '{key}'가 없어서 추가하였습니다.")
+            return 1
+    else: # 키 있으면 해당키의 최근 페이지 반환
+        if data[key]:
+            return max(int(k) for k in data[key].keys()) +1 , data     # 있으면 다음페이지랑 data 반환
+        else:
+            return 1
+        
+####
+print('test')
+def write_txt_log(T_path, text):
+    current_time = datetime.now()
+    formatted_time = current_time.strftime('%Y-%m-%d %H:%M:%S')
+
+    with open(T_path, 'a', encoding='utf-8') as file:
+        file.write(f"{formatted_time} ::: {text}\n")
+
+def write_json_log(J_PATH, data):
+    with open(J_PATH, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=4)
+    
+#####################################################################################################
+        
+
+
+
+def get_response(P=50): #    해야하는 페이지 받아서 return item_li 반환
+    print(P,'번 반복예정')
+    TXT_LOG_PATH = r'C:\Users\oem\Desktop\jhy\signlanguage\Sign_Language_Remaster\logs\LOG.TXT'
+    JSON_LOG_PATH = r'C:\Users\oem\Desktop\jhy\signlanguage\Sign_Language_Remaster\logs\api_log.json'
+    for _ in range(P):
+        subject = 'Daily'
+        todo_page, j_data = page_todo(JSON_LOG_PATH, subject)
+
+        url = 'http://api.kcisa.kr/openapi/service/rest/meta13/getCTE01701'
+        params = {
+            'serviceKey': 'ecc7282e-731e-4aa0-91b1-017535926c8f',
+            'numOfRows': 10,
+            'pageNo': todo_page,
+        }
+        response = requests.get(url, params=params)
+
+        if response.status_code == 200:
+            print('요청 성공')
+            write_txt_log(TXT_LOG_PATH, f'Page {todo_page} api 요청 성공')
+            page_start_time = datetime.now()
+            content_type = response.headers.get('Content-Type')
+            if content_type and 'charset' in content_type:
+                encoding = content_type.split('charset=')[-1].strip()
+            else:
+                encoding = 'utf-8'
+
+            xml_dict = xmltodict.parse(response.text, encoding=encoding)
+            json_response = json.dumps(xml_dict, ensure_ascii=False, indent=2)
+            json_response = json.loads(json_response)
+            # print('응답 내용:', json_response)
+            # print(type( json_response['response']['body']['items']['item']), json_response['response']['body']['items']['item'])
+            item_li = json_response['response']['body']['items']['item']
+            data_to_log = j_data
+            data_to_log[subject][todo_page] = dict()
+            for i, item in enumerate(item_li):
+                item_start_time = datetime.now()
+                print(item['title'], f'No.{i +1} in Page {todo_page}')
+                make_data(item['title'],item['subDescription'] )
+                item_end_time = datetime.now()
+                write_txt_log(TXT_LOG_PATH, f'\t{i+1}. {item["title"]} 작성완료 (걸린시간 {item_end_time - item_start_time})')
+                data_to_log[subject][todo_page][trans_to_english(item['title'])] = [item['title'],item['subDescription']]
+                write_json_log(JSON_LOG_PATH, data_to_log)
+            page_end_time = datetime.now()
+            write_txt_log(TXT_LOG_PATH, f'Page {todo_page} 작성 완료 (페이지 완료까지 걸린시간 {page_end_time - page_start_time})')
+    
+        else:
+            print(f'요청이 실패했습니다. 응답 코드: {response.status_code}')
+            write_txt_log(TXT_LOG_PATH, f'Page {todo_page} api 요청 실패\n 응답 코드: {response.status_code}')
+            break
+
+
+
+
+
+import sys
+
+if __name__ == "__main__":
+    file_start_time = datetime.now()
+    
+    if len(sys.argv)==2:
+        get_response(int(sys.argv[1]))
+    else:
+        get_response()
+    file_end_time = datetime.now()
+    elapsed_time = file_end_time - file_start_time
+    print(f"Generator 시작 시간: {file_start_time}")
+    print(f"Generator 끝난 시간: {file_end_time}")
+    print(f"Generator 걸린 시간: {elapsed_time}") 
+
